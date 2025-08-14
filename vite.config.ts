a91fc834f0ea822913173288a82bdada6e2a2275
Avery Lim <vivianaverylim@gmail.com>
2025-08-14 09:34:24 +0900

    try deployment again

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/ynot-advisory/" : "/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
